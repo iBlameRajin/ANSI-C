d60fb43bab6398be7ba49d63d99dca492b53368e
iBlameRajin <u2402052@student.cuet.ac.bd>
2026-05-13 18:06:47 +0600

    Create example7.4(a).c

---
+++ example7,4(a).c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+
+    int a[4][3];
+    for( int i=0;i<4;i++){
+        for(int j=0;j<3;j++){
+            scanf("%d",&a[i][j]);
+        }
+    }
+    int sum;
+   for(int m=0;m<4;m++,sum=0){
+    for(int n=0;n<3;n++){
+        sum=sum+a[m][n];
+
+    } printf("The total sale of the girl%d is %d\n",m+1,sum);
+   }
+return 0;}
