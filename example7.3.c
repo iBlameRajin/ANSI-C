6c71b1132d8c89f46f6aab137c10ef105b328e2d
iBlameRajin <u2402052@student.cuet.ac.bd>
2026-05-13 16:49:57 +0600

    Update example7.3.c

--- example7.3.c
+++ example7.3.c
@@ -1,22 +1,19 @@
 #include<stdio.h>
 #include<string.h>
-
 int main(){
-char n[100];
+    char n[100];
     scanf("%s",n);
     int l=strlen(n);
     int j=l-1;
     while(n[j]!='1'){
         j--;
     }
-    int i=j-1;
-    while(i>=0){
-        if(n[i]=='1')
-            n[i]='0';
-        else
-            n[i]='1';
-    i--;}
- for(int i=0;i<l;i++)
-    printf("%c",n[i]);
+   for( int i=j-1;i>=0;i--){
+    if(n[i]=='1')
+        n[i]='0';
+    else
+        n[i]='1';
+   }
+   printf("%s",n);
 
 return 0;}
