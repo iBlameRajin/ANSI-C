95c31a144f4a53c28fe5174f43f31fba48b72166
MD. RAJIN SHALEH <u2402052@student.cuet.ac.bd>
2026-05-15 00:37:59 +0600

    Create exer6.20.c

---
+++ chapter6/exer6.20.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int sum=0;
+    int n;
+    int count=1;
+    for(int i=1;i<=10,sum<=999;i++){
+        scanf("%d",&n);
+        if(n>0){
+            sum=sum+n;
+            count++;
+    }
+    printf("%d %d",count,sum);
+return 0;}
