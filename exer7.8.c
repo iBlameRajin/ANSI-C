753721060e80d297ed4212deee570d34043f74ff
MD. RAJIN SHALEH <u2402052@student.cuet.ac.bd>
2026-05-14 12:25:45 +0600

    Create exer7.8.c

---
+++ exer7.8.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main(){
+    int a[5][5];
+    for(int i=0;i<5;i++){
+        for(int j=0;j<5;j++){
+            if(i==j)
+                a[i][j]=0;
+            else if(i<j)
+                a[i][j]=+1;
+            else
+                a[i][j]=-1;
+        }
+    }
+    for(int i=0;i<5;i++){
+        for(int j=0;j<5;j++){
+            printf("%d ",a[i][j]);
+        }
+        printf("\n");
+    }
+return 0;}
