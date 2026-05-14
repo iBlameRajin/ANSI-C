455c949cd03422d4f2cb676421fa90e469c49260
MD. RAJIN SHALEH <u2402052@student.cuet.ac.bd>
2026-05-14 20:21:07 +0600

    Create exer7,15.c

---
+++ exer7,15.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+int main(){
+    int a[3][3];
+    int b[3][3];
+    int sum[3][3];
+    int sub[3][3];
+    for(int i=0;i<3;i++){
+        for(int j=0;j<3;j++){
+            scanf("%d",&a[i][j]);
+        }
+    }
+    for(int i=0;i<3;i++){
+        for(int j=0;j<3;j++){
+            scanf("%d",&b[i][j]);
+        }
+    }
+    for(int i=0;i<3;i++){
+        for(int j=0;j<3;j++){
+            sum[i][j]=a[i][j]+b[i][j];
+        }
+    }
+     for(int i=0;i<3;i++){
+        for(int j=0;j<3;j++){
+            sub[i][j]=a[i][j]-b[i][j];
+        }
+    }
+     for(int i=0;i<3;i++){
+        for(int j=0;j<3;j++){
+            printf("%d",sum[i][j]);
+        }printf("\n");
+        }
+        for(int i=0;i<3;i++){
+        for(int j=0;j<3;j++){
+            printf("%d",sub[i][j]);
+        }printf("\n");
+        }
+return 0;}
