a6a9189781c9e38881e90ba659c679635ef59eb4
MD. RAJIN SHALEH <u2402052@student.cuet.ac.bd>
2026-05-17 01:03:16 +0600

    Create trapzoidal.c

---
+++ NUMERICAL/trapzoidal.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<math.h>
+#define f(x) pow(x,3)+2*x+1
+int main(){
+  float a,b,h,x,sum;
+  int n;
+  printf("Enter the upper and lower limit");
+  scanf("%f %f",&b,&a);
+  printf("Enter the number of sub interval");
+  scanf("%d",&n);
+  h=(b-a)/n;
+  sum=0;
+  sum=f(a)+f(b);
+  for(int i=1;i<n;i++){
+    x=a+i*h;
+    
+        sum+=2*f(x);
+   
+  }
+  printf("the result is %f",(h*sum)/2);
+return 0;}
