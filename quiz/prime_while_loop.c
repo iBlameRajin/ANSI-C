336373bd835407be877f180774977ea15ba67b1b
MD. RAJIN SHALEH <u2402052@student.cuet.ac.bd>
2026-05-17 21:33:07 +0600

    Create prime_while_loop.c

---
+++ quiz/prime_while_loop.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+    int n;
+    int a=0;
+    scanf("%d",&n);
+   int i=2;
+   while(i<n){
+        if(n%i==0){
+            a=1;
+            break;}
+            i++;
+    }
+    if(a==1)
+        printf("This is a composite number");
+    else
+        printf("This is a prime number");
+return 0;}
