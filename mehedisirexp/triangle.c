0c712c6645181d7b0ccbb193494620bdf9d8b2e2
MD. RAJIN SHALEH <u2402052@student.cuet.ac.bd>
2026-05-17 14:22:24 +0600

    Create triangle.c

---
+++ mehedisirexp/triangle.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+    float a,b,c;
+    printf("Enter the three side of the triangle");
+    scanf("%f %f %f",&a,&b,&c);
+    if(a+b>c && b+c>a && c+a>b){
+        if(a==b && b==c)
+            printf("the triangle is equilataral");
+        else if(a==b || b==c)
+            printf("The triangle is isoscales");
+        else
+            printf("The triangle is scalane");
+    }
+    else
+        printf("no triangle is possible");
+return 0;}
