32f45821a3d64dd727b23eb8803c46089c419316
MD. RAJIN SHALEH <u2402052@student.cuet.ac.bd>
2026-05-17 16:02:59 +0600

    Create squarealphabet.c

---
+++ mehedisirexp/squarealphabet.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    int a=65;
+    for(int i=1;i<=5;i++){
+        for(int j=1;j<=5;j++){
+            printf("%c ",a);
+            a++;
+        }printf("\n");
+    }
+return 0;}
